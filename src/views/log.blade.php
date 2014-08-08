30602d13b318fef597c45a76173c651ef2154f13
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-08-08 16:17:45 -0300

    Cleaning

--- src/views/log.blade.php
+++
@@ -1,7 +0,0 @@
-@extends(Config::get('pragmarx/sdk::stats_layout'))
-
-@section('page-contents')
-	@include('pragmarx/sdk::_dataTable', array('route' => route('sdk.stats.api.log', array('uuid' => $uuid))))
-
-	<div id='table_div'></div>
-@stop
