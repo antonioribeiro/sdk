6a49067f49a9a72a00d495a270ce4e1519e6d0c6
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-12-10 10:06:47 -0200

    Add Config Service

---
+++ src/Services/Config/Service/Config.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace PragmaRX\Sdk\Services\Config\Service;
+
+use ArrayAccess;
+
+class Config implements ArrayAccess {
+
+	protected $packages = [];
+
+	public function __construct()
+	{
+	    $this->config = app()->make('config');
+	}
+
+	public function get($key, $default = null)
+	{
+		$keys = [];
+
+		list($namespace, $key) = $this->explodeKey($key);
+
+		if ($namespace)
+		{
+			$keys = $this->loadPackageConfig($namespace);
+		}
+
+		if ( ! $keys)
+		{
+			// Use Laravel's config
+			return $this->config->get($key, $default);
+		}
+
+		if ($keys && is_array($keys))
+		{
+			$keys = array_get($keys, $key, $default);
+		}
+
+		return $keys;
+	}
+
+	public function __call($name, $arguments)
+	{
+		return call_user_func_array(
+			array($this->config, $name),
+			$arguments
+		);
+	}
+
+	private function explodeKey($key)
+	{
+		$exploded = explode('::', $key);
+
+		if (count($exploded) == 1)
+		{
+			$exploded = ['', $exploded[0]];
+		}
+
+		return $exploded;
+	}
+
+	private function loadPackageConfig($namespace)
+	{
+		if (isset($this->packages[$namespace]))
+		{
+			return $this->packages[$namespace];
+		}
+
+		$paths = [
+			base_path().
+				DIRECTORY_SEPARATOR.'config'.
+				DIRECTORY_SEPARATOR.'packages'.
+				DIRECTORY_SEPARATOR.$namespace.
+				DIRECTORY_SEPARATOR.'config.php',
+
+			base_path().
+				DIRECTORY_SEPARATOR.'vendor'.
+				DIRECTORY_SEPARATOR.$namespace.
+				DIRECTORY_SEPARATOR.'src'.
+				DIRECTORY_SEPARATOR.'config'.
+				DIRECTORY_SEPARATOR.'config.php',
+		];
+
+		foreach($paths as $path)
+		{
+			if (file_exists($path))
+			{
+				$keys = require $path;
+
+				$this->packages[$namespace] = $keys;
+
+				return $keys;
+			}
+		}
+	}
+
+	public function offsetExists($key)
+	{
+		return $this->has($key);
+	}
+
+	public function offsetGet($key)
+	{
+		return $this->get($key);
+	}
+
+	public function offsetSet($key, $value)
+	{
+		$this->set($key, $value);
+	}
+
+	public function offsetUnset($key)
+	{
+		$this->set($key, null);
+	}
+
+	public function package($a, $b = null, $c = null, $e = null)
+	{
+		return $a = $b;
+	}
+
+}
---
+++ src/Services/Config/Service/Provider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PragmaRX\Sdk\Services\Config\Service;
+
+use PragmaRX\Support\ServiceProvider;
+
+class Provider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerConfig();
+	}
+
+	private function registerConfig()
+	{
+		// Instantiate and copy Laravel's config
+		$config = new Config();
+
+		// Override Laravel's config IoC binding
+		$this->app->bindShared('config', function($app) use ($config)
+		{
+			return $config;
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return ['pragmarx.config', 'config'];
+	}
+
+}
