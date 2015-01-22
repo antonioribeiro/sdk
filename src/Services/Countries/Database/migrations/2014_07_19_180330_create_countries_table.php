75133e45ac3b9f9c29b517508ed27f734f67764b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-01-22 12:16:31 -0200

    Do some seed

--- src/Services/Countries/Database/migrations/2014_07_19_180330_create_countries_table.php
+++ src/Services/Countries/Database/migrations/2014_07_19_180330_create_countries_table.php
@@ -22,6 +22,11 @@ class CreateCountriesTable extends Migration {
 			$table->float('longitude')->nullable();
 			$table->timestamps();
 		});
+
+		Country::create(['name' => 'Brazil', 'code' => 'BR', 'currency_id' => $currencies[0]->id]);
+		Country::create(['name' => 'Swiss', 'code' => 'CH', 'currency_id' => $currencies[1]->id]);
+		Country::create(['name' => 'United States', 'code' => 'US', 'currency_id' => $currencies[2]->id]);
+		Country::create(['name' => 'Italy', 'code' => 'IT', 'currency_id' => $currencies[3]->id]);
 	}
 
 
--- src/Services/Currencies/Database/migrations/2014_07_19_180320_create_currencies_table.php
+++ src/Services/Currencies/Database/migrations/2014_07_19_180320_create_currencies_table.php
@@ -2,8 +2,11 @@
 
 use PragmaRX\Support\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use PragmaRX\Sdk\Services\Currencies\Data\Entities\Currency;
 
 class CreateCurrenciesTable extends Migration {
+
+	public static $generateId = false;
 
 	/**
 	 * Run the migrations.
@@ -16,13 +19,19 @@ class CreateCurrenciesTable extends Migration {
 		{
 			$table->string('id', 64)->primary();
 			$table->string('name');
-			$table->string('code', 5);
+			$table->string('code', 5)->index();
 			$table->string('symbol');
 			$table->integer('decimals')->unsigned();
 			$table->string('decimal_point');
 			$table->string('thousands_separator');
 			$table->timestamps();
 		});
+
+		Currency::create(['id' => 'BRL', 'name' => 'Real', 'code' => 'BRL', 'symbol' => 'R$', 'decimals' => 2, 'decimal_point' => ',', 'thousands_separator' => '.']);
+		Currency::create(['id' => 'USD', 'name' => 'US Dollar', 'code' => 'USD', 'symbol' => '$', 'decimals' => 2, 'decimal_point' => '.', 'thousands_separator' => ',']);
+		Currency::create(['id' => 'EUR', 'name' => 'Euro', 'code' => 'EUR', 'symbol' => '€', 'decimals' => 2, 'decimal_point' => '.', 'thousands_separator' => ',']);
+		Currency::create(['id' => 'CHF', 'name' => 'Swiss franc', 'code' => 'CHF', 'symbol' => 'SFr.', 'decimals' => 2, 'decimal_point' => '.', 'thousands_separator' => "'"]);
+		Currency::create(['id' => 'GPB', 'name' => 'Pound', 'code' => 'GBP', 'symbol' => '£', 'decimals' => 2, 'decimal_point' => '.', 'thousands_separator' => ',']);
 	}
 
 
