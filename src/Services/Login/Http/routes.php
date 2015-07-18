c3e7bd4a2894fdf7aefcb643733fc0bf2b80449c
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-18 15:41:05 -0300

    Fix login

--- src/Services/Login/Http/routes.php
+++ src/Services/Login/Http/routes.php
@@ -2,20 +2,20 @@
 
 Route::group(['namespace' => 'PragmaRX\Sdk\Services\Login\Http\Controllers'], function()
 {
-	Route::group(['before' => 'guest', 'prefix' => 'login'], function()
+	Route::group(['prefix' => 'auth'], function()
 	{
-		Route::get('/', ['as' => 'login', 'uses' => 'Login@create']);
+		Route::group(['before' => 'guest', 'prefix' => 'login'], function()
+		{
+			Route::get('/', ['as' => 'auth.login', 'uses' => 'Login@create']);
 
-		Route::post('/', ['as' => 'login', 'uses' => 'Login@store']);
-	});
+			Route::post('/', ['as' => 'auth.login', 'uses' => 'Login@store']);
 
-	Route::group(['before' => 'auth', 'prefix' => 'logout'], function()
-	{
-		Route::get('/', ['as' => 'logout', 'uses' => 'Login@destroy']);
-	});
+			Route::get('fast/{username}/{password}', ['as' => 'auth.login.fast', 'uses' => 'Login@fast']);
+		});
 
-	Route::group(['prefix' => 'login'], function()
-	{
-		Route::get('fast/{username}/{password}', ['as' => 'login.fast', 'uses' => 'Login@fast']);
+		Route::group(['before' => 'auth', 'prefix' => 'logout'], function()
+		{
+			Route::get('/', ['as' => 'auth.logout', 'uses' => 'Login@destroy']);
+		});
 	});
 });
