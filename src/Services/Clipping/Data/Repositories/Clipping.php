05fa5e1007c12e7ee2d62c98201c87133e8b6413
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-02 21:55:38 -0300

    Add article

--- src/Services/Clipping/Data/Repositories/Clipping.php
+++ src/Services/Clipping/Data/Repositories/Clipping.php
@@ -2,7 +2,16 @@
 
 namespace PragmaRX\Sdk\Services\Clipping\Data\Repositories;
 
+use Carbon\Carbon;
 use PragmaRX\Sdk\Services\Clipping\Data\Entities\Clipping as Model;
+
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\Clipping as ClippingArticle;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingAuthor;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingCategory;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingFile;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingFileType;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingLocality;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingVehicle;
 
 class Clipping
 {
@@ -16,12 +25,62 @@ class Clipping
 		return Model::findOrFail($id);
 	}
 
-	public function add($data)
+	public function add($article)
 	{
-		echo "done?\n";
+		$publishedAt = Carbon::createFromFormat('m/d/Y', $article['published_at']);
 
-		dd($data);
+		$authorId = ClippingAuthor::firstOrCreate(['name' => $article['author']]);
 
-		return $data;
+		if ( ! $vehicleId = $article['vehicle_id'])
+		{
+			$vehicleId = ClippingVehicle::firstOrCreate(['name' => $article['vehicle_other']])->id;
+		}
+
+		if ( ! $categoryId = $article['category_id'])
+		{
+			$categoryId = ClippingCategory::firstOrCreate(['name' => $article['category_other']])->id;
+		}
+
+		if ( ! $localityId = $article['locality_id'])
+		{
+			$localityId = ClippingLocality::firstOrCreate(['name' => $article['locality_other']]);
+		}
+
+		$clipping = ClippingArticle::create(
+			[
+				'heading' => $article['heading'],
+				'subheading' => $article['subheading'],
+				'body' => $article['body'],
+				'author_id' => $authorId,
+				'vehicle_id' => $vehicleId,
+				'category_id' => $categoryId,
+				'locality_id' => $localityId,
+				'url' => $article['url'],
+				'article_preview_url' => $article['image_snapshot_urls'],
+				'published_at' => $publishedAt,
+			]
+		);
+
+		$this->createImages($clipping->id, $article['image_main_urls'], true); // main
+		$this->createImages($clipping->id, $article['image_snapshot_urls'], false, true); // snapshot
+		$this->createImages($clipping->id, $article['image_other_urls']); // other images
+
+		return $clipping;
+	}
+
+	private function createImages($clippingId, $urls, $main = false, $snapshot = false)
+	{
+		$clippingFile = app()->make(ClippingFile::class);
+
+		foreach ($urls as $url)
+		{
+			$clippingFile->createFor(
+				$clippingId,
+				$main,
+				$snapshot,
+				$url,
+				ClippingFileType::firstorCreate(['name' => 'image'])
+			);
+		}
 	}
 }
