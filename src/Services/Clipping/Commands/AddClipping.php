ff0de7098eb5fd0c8c5f08e18897101acf7dbf53
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-07 15:00:58 -0300

    Fix Bus

--- src/Services/Clipping/Commands/AddClipping.php
+++ src/Services/Clipping/Commands/AddClipping.php
@@ -41,6 +41,45 @@ class AddClipping extends SelfHandlingCommand {
 
 	public $tags;
 
+	public function __construct(
+						$url,
+						$published_at,
+						$heading,
+						$subheading,
+						$body,
+						$editorial_id,
+						$editorial_other,
+						$vehicle_id,
+						$vehicle_other,
+						$author,
+						$locality_id,
+						$locality_other,
+						$image_main_urls,
+						$image_snapshot_urls,
+						$image_other_urls,
+						$videos,
+						$tags
+	)
+	{
+		$this->url = $url;
+		$this->published_at = $published_at;
+		$this->heading = $heading;
+		$this->subheading = $subheading;
+		$this->body = $body;
+		$this->editorial_id = $editorial_id;
+		$this->editorial_other = $editorial_other;
+		$this->vehicle_id = $vehicle_id;
+		$this->vehicle_other = $vehicle_other;
+		$this->author = $author;
+		$this->locality_id = $locality_id;
+		$this->locality_other = $locality_other;
+		$this->image_main_urls = $image_main_urls;
+		$this->image_snapshot_urls = $image_snapshot_urls;
+		$this->image_other_urls = $image_other_urls;
+		$this->videos = $videos;
+		$this->tags = $tags;
+	}
+
 	public function handle(ClippingRepository $clippingRepository)
 	{
 		$this->dispatchEventsFor(
