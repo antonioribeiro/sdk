e63ab1625e53880af821095b45d75c7037b0431e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-10-04 23:01:03 -0300

    Fix styling

--- src/Services/Bus/Commands/Command.php
+++ src/Services/Bus/Commands/Command.php
@@ -4,13 +4,12 @@ namespace PragmaRX\Sdk\Services\Bus\Commands;
 
 use PragmaRX\Sdk\Services\Bus\Events\DispatchableTrait;
 
-abstract class Command {
-
+abstract class Command
+{
 	use DispatchableTrait;
 
 	public function getPublicProperties()
 	{
 		return get_object_vars($this);
 	}
-
 }
