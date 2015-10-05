3c674b6284954282a0c5c1782bd2eb940c88ff87
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-10-04 23:01:50 -0300

    Rename command

---
+++ src/Services/Products/Commands/AddProduct.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PragmaRX\Sdk\Services\Products\Commands;
+
+use PragmaRX\Sdk\Services\Bus\Commands\SelfHandlingCommand;
+use PragmaRX\Sdk\Services\Products\Data\Repositories\Products as ProductsRepository;
+
+class AddProduct extends SelfHandlingCommand
+{
+	public $name;
+
+	public $description;
+
+	public $cost;
+
+	public $price;
+
+	public $stock;
+
+	public function handle(ProductsRepository $productsRepository)
+	{
+		$this->dispatchEventsFor(
+			$result = $productsRepository->add($this->getPublicProperties())
+		);
+
+		return $result;
+	}
+}
