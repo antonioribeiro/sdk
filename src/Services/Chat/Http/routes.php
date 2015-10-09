f25819a322edad3aa6cdadfc6a7b603863ff978e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-10-08 23:57:29 -0300

    Rename routes

--- src/Services/Chat/Http/routes.php
+++ src/Services/Chat/Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['prefix' => 'chat', 'namespace' => 'PragmaRX\Sdk\Services\Chat\Http\Controllers'], function ()
 {
-	Route::get('/', ['as' => 'block', 'uses' => 'Chat@index']);
+	Route::get('/', ['as' => 'chat.index', 'uses' => 'Chat@index']);
 
-	Route::get('chat/send/{username}/{message}', ['as' => 'block', 'uses' => 'Chat@sendMessage']);
+	Route::get('chat/send/{username}/{message}', ['as' => 'chat.send.message', 'uses' => 'Chat@sendMessage']);
 });
