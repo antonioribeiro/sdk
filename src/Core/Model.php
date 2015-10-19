eced8e5e733506cb856c8989bcda17d8eb58fe06
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-10-19 12:36:46 -0200

    Create revisions for every save

--- src/Core/Model.php
+++ src/Core/Model.php
@@ -2,13 +2,16 @@
 
 namespace PragmaRX\Sdk\Core;
 
-use PragmaRX\Sdk\Services\Bus\Events\EventGenerator;
 use PragmaRX\Sdk\Core\Traits\ReloadableTrait;
 use PragmaRX\Sdk\Core\Traits\IdentifiableTrait;
+use Venturecraft\Revisionable\RevisionableTrait;
 use Illuminate\Database\Eloquent\Model as Eloquent;
+use PragmaRX\Sdk\Services\Bus\Events\EventGenerator;
 use PragmaRX\Sdk\Services\Presenter\PresentableTrait;
 
-class Model extends Eloquent {
+class Model extends Eloquent
+{
+	use RevisionableTrait;
 
 	protected $dates = ['created_at', 'updated_at'];
 
