e40087986850c180835ec85d5b1dbb047241c631
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-11-14 17:29:31 +0000

    Fix coding style

--- src/Services/Accounts/Commands/SignInCommand.php
+++ src/Services/Accounts/Commands/SignInCommand.php
@@ -5,36 +5,33 @@ namespace PragmaRX\Sdk\Services\Accounts\Commands;
 use PragmaRX\Sdk\Services\Bus\Commands\SelfHandlingCommand;
 use PragmaRX\Sdk\Services\Users\Data\Repositories\UserRepository;
 
-class SignInCommand extends SelfHandlingCommand {
+class SignInCommand extends SelfHandlingCommand
+{
+    public $email;
 
-	public $email;
+    public $password;
 
-	public $password;
+    public $remember;
 
-	public $remember;
+    function __construct($email, $password, $remember) {
+        $this->email = $email;
 
-	function __construct($email, $password, $remember)
-	{
-		$this->email = $email;
+        $this->password = $password;
 
-		$this->password = $password;
+        $this->remember = $remember;
+    }
 
-		$this->remember = $remember;
-	}
+    public function handle(UserRepository $userRepository) {
+        $credentials = [
+            'email'    => $this->email,
+            'password' => $this->password,
+            'remember' => $this->remember,
+        ];
 
-	public function handle(UserRepository $userRepository)
-	{
-		$credentials = [
-			'email' => $this->email,
-			'password' => $this->password,
-			'remember' => $this->remember
-		];
+        $result = $userRepository->authenticate($credentials);
 
-		$result = $userRepository->authenticate($credentials);
+        $this->dispatchEventsFor($result['user']);
 
-		$this->dispatchEventsFor($result['user']);
-
-		return $result;
-	}
-
+        return $result;
+    }
 }
