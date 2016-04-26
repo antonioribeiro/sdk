bce25aceae8e3fb911c621dd578862baa38ae6c8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-04-25 21:21:45 -0300

    User spinner

--- src/Services/Telegram/Data/Presenters/TelegramMessage.php
+++ src/Services/Telegram/Data/Presenters/TelegramMessage.php
@@ -12,7 +12,7 @@ class TelegramMessage extends Presenter
      */
     private function defaultSpinner()
     {
-        return '<img class="kallzenter-chat-telegram-photo" src="' . config('chat.spinner_image') . '" />';
+        return '<img class="kallzenter-chat-telegram-photo" src="' . asset('assets/images/chat/spiner.gif') . '" />';
     }
 
     private function makeDocumentMessage()
