a153ded73d871a115baf25a76bf853be03faddf8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-08-28 19:55:40 +0000

    Add cities relation

--- src/Services/States/Data/Entities/State.php
+++ src/Services/States/Data/Entities/State.php
@@ -3,9 +3,10 @@
 namespace PragmaRX\Sdk\Services\States\Data\Entities;
 
 use PragmaRX\Sdk\Core\Database\Eloquent\Model;
+use PragmaRX\Sdk\Services\Cities\Data\Entities\City;
 
-class State extends Model {
-
+class State extends Model
+{
 	protected $fillable = [
 		'code',
 		'name',
@@ -17,4 +18,8 @@ class State extends Model {
 		return $this->belongsTo('PragmaRX\Sdk\Services\Countries\Data\Entities\Country');
 	}
 
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+	}
 }
